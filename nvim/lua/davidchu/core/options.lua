3f6e68c20e73c7f64f6266887939b93a5225bfa6
David Chu Kuong Io <david@marstree.ltd>
2023-07-29 19:41:05 +0800

    david: update vim autoread when outside edit file

--- nvim/lua/davidchu/core/options.lua
+++ nvim/lua/davidchu/core/options.lua
@@ -32,4 +32,13 @@ vim.cmd([[
   let @/=""
 
   filetype plugin indent on
+
+  if ! exists("g:CheckUpdateStarted")
+    let g:CheckUpdateStarted=1
+    call timer_start(1,'CheckUpdate')
+  endif
+  function! CheckUpdate(timer)
+    silent! checktime
+    call timer_start(1000,'CheckUpdate')
+  endfunction
 ]])
