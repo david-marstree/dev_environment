a2fa60538a06a514c4246579ed3df9131b467d61
David Chu Kuong Io <david@marstree.ltd>
2023-10-17 17:18:06 +0800

    nvim for obsidian

--- nvim/lua/davidchu/plugins/obsidian.lua
+++ nvim/lua/davidchu/plugins/obsidian.lua
@@ -6,6 +6,16 @@ end
 
 
 obsidian.setup({
-    dir = '~/obsidian/david-obsidian'
-})
+    dir = '~/obsidian/david-obsidian',
 
+    mappings = {
+        ["gf"] = {
+            action = function()
+                return obsidian.util.gf_passthrough()
+            end,
+            opts = { noremap = false , expr = true, buffer = true },
+        },
+    },
+
+
+})
