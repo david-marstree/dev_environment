f764dfc81f50fc5761d46e56262391badd3e5ed3
David <david.kl.chu@boardware.com>
2024-03-05 01:58:57 +0800

    david: remove nvim and vim config

--- nvim/lua/davidchu/core/colorscheme.lua
+++
@@ -1,9 +0,0 @@
--- set colorscheme
--- in case it isnt installed
-local colorscheme = "solarized8"
-
-local status, _ = pcall(vim.cmd, string.format("colorscheme %s", colorscheme))
-if not status then
-  print("Colorscheme " .. colorscheme .. " not found!")
-  return
-end
